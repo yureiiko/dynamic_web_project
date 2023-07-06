6633433e6e48c74f6e4803f8c27adc7d2d0b6b7b
yureiiko <101016323+yureiiko@users.noreply.github.com>
2023-07-06 09:41:09 +0100

    Update database_creation.sql

--- database_creation.sql
+++ database_creation.sql
@@ -18,29 +18,35 @@ create table seller (
 
 create table cart (
     id_cart integer primary key,
-    id_buyer integer foreign key references buyer(id_buyer)
+    id_buyer integer,
+    foreign key (id_buyer) references buyer(id_buyer)
 );
 
 create table product (
     id_prod integer primary key,
     img_src varchar(30),
     descrip varchar(60),
-    id_seller integer foreign key references seller(id_seller),
-    id_cart integer foreign key references cart(id_cart)
+    id_seller integer,
+    foreign key (id_seller) references seller(id_seller),
+    id_cart integer,
+    foreign key (id_cart) references cart(id_cart)
 );
 
 create table BIN (
     id_bin integer primary key,
     price integer not null,
-    id_prod integer foreign key references product(id_prod)
+    id_prod integer,
+    foreign key (id_prod) references product(id_prod)
 );
 
 create table auction (
     id_auc integer primary key,
     max_price integer,
-    price integer
-    id_prod integer foreign key references product(id_prod),
-    id_buyer integer foreign key references buyer(id_buyer) 
+    price integer,
+    id_prod integer,
+    foreign key (id_prod) references product(id_prod),
+    id_buyer integer,
+    foreign key (id_buyer) references buyer(id_buyer) 
 );
 
 create table best_offer (
@@ -48,12 +54,15 @@ create table best_offer (
     buyer_price integer,
     seller_price integer,
     price integer,
-    id_prod integer foreign key references product(id_prod),
-    id_buyer integer foreign key references buyer(id_buyer) 
+    id_prod integer,
+    foreign key (id_prod) references product(id_prod),
+    id_buyer integer,
+    foreign key (id_buyer) references buyer(id_buyer) 
 );
 
 create table bid_chat (
     id_offer integer primary key,
     offer integer not null,
-    id_bo integer foreign key references best_offer(id_bo)
+    id_bo integer,
+    foreign key (id_bo) references best_offer(id_bo)
 );
