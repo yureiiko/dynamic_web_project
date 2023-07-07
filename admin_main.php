9f6626479f727199622cc29467ffb918483be3bf
yureiiko <101016323+yureiiko@users.noreply.github.com>
2023-07-07 14:28:13 +0100

    Add files via upload

---
+++ admin_main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Grandeur Estates & Cars : Admin Main Page</title>
+	</head>
+	<body>
+		<h1>Welcome Admin</h1>
+		<span><h4>Products</h4></span><span><h4>Buyers</h4></span><span><h4>Sellers</h4></span>
+		
+	</body>
+</html>
