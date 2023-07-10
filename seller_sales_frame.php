cac668f8447728eaf6cb9e26867784714c849924
yureiiko <101016323+yureiiko@users.noreply.github.com>
2023-07-10 17:54:04 +0100

    Update seller_sales_frame.php
    Seller can see her/his previous sales.

--- seller_sales_frame.php
+++ seller_sales_frame.php
@@ -10,7 +10,6 @@
 		if (!isset($_COOKIE["seller"])) {
 			header("Location: login.php");
 		}
-        echo("<br>".$_COOKIE["seller"]."<br>");
 		?>
         <h4>Sales</h4>
 		<?php
@@ -19,18 +18,13 @@
         $database = "dynamic_web_project";
         $conn = new mysqli("localhost", $usr, $password, $database);
         if ($conn->connect_error) {
-            die
+            echo "db error <br>";
         }
-        $query = "select * from sales";
+        $query = "select p.img_src, p.descrip, b.username from product p, buyer b, sales s where p.id_prod=s.id_prod and p.id_seller=1 and b.id_buyer=s.id_buyer";
 		$res = mysqli_query($conn, $query);
 		mysqli_close($conn);
-        //echo "<br>".$res."<br>";
-        if (!$res===false) {
-            while ($row = mysqli_fetch_array($res)) {
-                echo "<div>".$row["id_seller"]." <img src='".$row["img_src"]."'> ".$row["descrip"]." Sold to ".$row["username"]."<div>";
-            }
-        } else {
-            echo "TEST error"
+        while ($row = mysqli_fetch_array($res)) {
+            echo "<div>".$row["id_seller"]." <img src='".$row["img_src"]."'> ".$row["descrip"]." Sold to ".$row["username"]."<div>";
         }
         ?>
         <span id="del"></span>
